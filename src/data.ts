ee4dbd00f53cae79beefbdde1d210c02532d0dba
Taiwo Damilola <tdammy92@gmail.com>
2024-05-16 14:55:43 +0100

    fix: revert settings

---
+++ src/data.ts
@@ -0,0 +1,65 @@
+export const Feeds = [
+    {
+      "id": 1,
+      "username": "Rachel",
+      "type": "video",
+      "caption": "Career and growth",
+      "videoUrl": "https://youtu.be/0XWzqUptsYU",
+      "imageUrl": "",
+      "thumbnailUrl": "https://randomuser.me/api/portraits/women/2.jpg",
+      "likes": 3982,
+      "comments": 289,
+      "shares": 176,
+      "timestamp": "2024-05-13T15:45:00Z"
+    },
+    {
+      "id": 2,
+      "username": "Damien",
+      "type": "image",
+      "caption": "Check out this amazing view! 🌅 #nature #travel",
+      "videoUrl": "",
+      "imageUrl": "https://cdn.outsideonline.com/wp-content/uploads/2023/06/hiker-waterfall-nature_s.jpg",
+      "thumbnailUrl": "https://randomuser.me/api/portraits/men/80.jpg",
+      "likes": 2567,
+      "comments": 134,
+      "shares": 98,
+      "timestamp": "2024-05-14T10:30:00Z"
+    },
+    {
+      "id": 3,
+      "username": "jackson",
+      "type": "video",
+      "caption": "Dancing to my favorite song! 💃🎶 #dance #music",
+      "imageUrl": "",
+      "videoUrl": "https://www.youtube.com/shorts/pKF73oHgf9k?feature=share",
+      "thumbnailUrl": "https://randomuser.me/api/portraits/men/46.jpg",
+      "likes": 3982,
+      "comments": 289,
+      "shares": 176,
+      "timestamp": "2024-05-13T15:45:00Z"
+    },
+    {
+      "id": 4,
+      "username": "Camela95",
+      "type": "video",
+      "caption": "Cooking up a storm in the kitchen! 🍳🔥 #cooking #foodie",
+      "videoUrl": "https://www.youtube.com/shorts/5ZPKf0jB3ZA?feature=share",
+      "imageUrl": "",
+      "thumbnailUrl": "https://randomuser.me/api/portraits/women/77.jpg",
+      "comments": 3245,
+      "shares": 2340,
+      "timestamp": "2024-05-12T20:00:00Z"
+    },
+    {
+      "id": 5,
+      "username": "Jane",
+      "type": "video",
+      "caption": "Tech is life",
+      "videoUrl": "https://videos.pexels.com/video-files/3209829/3209829-uhd_3840_2160_25fps.mp4",
+      "imageUrl": "",
+      "thumbnailUrl": "https://randomuser.me/api/portraits/women/89.jpg",
+      "comments": 3245,
+      "shares": 2340,
+      "timestamp": "2024-05-12T20:00:00Z"
+    }
+  ]
